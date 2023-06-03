d9a8b99ddbe9bb272875562bc9db78bb8ed178d2
BlUhOl <92349922+BlUhOl@users.noreply.github.com>
2023-06-03 13:36:18 +0700

    update link

--- src/components/mainhome.jsx
+++ src/components/mainhome.jsx
@@ -6,7 +6,7 @@ const mainhome = () => {
     <div className='bg-emerald-700 w-full h-screen p-2'>
       <h1 className='font-bold text-6xl'> Overview</h1>
 
-      <iframe className='w-full h-screen' src="https://docs.google.com/spreadsheets/d/1GUbu2P6J1zdd8j-AdCfkNMMZIbO1SWwYoWxwPXBJbqI/edit?usp=sharing?widget=true&amp;headers=false"></iframe>
+      <iframe className='w-full h-screen' src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSP4yaJCGZFJnogPaQbjl5yjjgUAW_lIYgaya1TwWDJWOsNfxicZe9poXLG9SXwdhWAnPE88imP8fUs/pubhtml?gid=2077112624&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
     </div>
     </main>
   )
