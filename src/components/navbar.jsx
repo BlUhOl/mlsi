8c7929ff072f183c98ebe0dc585fe445f0eb4a07
ChiefSY117 <kurniawannaufal431@gmail.com>
2023-06-27 08:39:34 +0700

    logout

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -32,7 +32,7 @@ const navbar = () => {
             </a>
 
             <a href='/'>
-                <div className='mt-96 items-center justify-center flex border-2 border-red-300 bg-red-100 text-xl rounded-xl p-2 hover:font-semibold hover:bg-red-500 hover:text-red-50 transition-all ease-in'>
+                <div className='mt-24 items-center justify-center flex border-2 border-red-300 bg-red-100 text-xl rounded-xl p-2 hover:font-semibold hover:bg-red-500 hover:text-red-50 transition-all ease-in'>
                     Logout
                 </div>
                 </a>
